99b9a6be927e6f499c50204dee54574bbb2779d0
lamadipen <lamadipen@yahoo.com>
2024-08-31 10:15:40 -0600

    [2D Dynamic Programming] LongestCommonSubsequence.py

---
+++ masteringAlgorithm/neetcode/pythonSolutions/2DDynamicProgramming/LongestCommonSubsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for r in range(len(text1) - 1, -1, -1):
+            for c in range(len(text2) - 1, -1, -1):
+                if text1[r] == text2[c]:
+                    dp[r][c] = 1 + dp[r+1][c+1]
+                else:
+                    dp[r][c] = max(dp[r][c+1], dp[r+1][c])
+            print(dp[r])
+
+        return dp[0][0]
+
+solution = Solution()
+actual1 =solution.longestCommonSubsequence('abcde', 'ace')
+print(actual1)
